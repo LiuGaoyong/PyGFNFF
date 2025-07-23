bd7d26fed9683907daaf5bd68d69ebad6b5f41db
LiuGaoyong <liugaoyong_88@163.com>
2025-07-23 16:23:48 +0800

    compile f2py module & import it successfully

--- fortran/_pygfnfflib.f90
+++ fortran/_pygfnfflib.f90
@@ -1,4 +1,4 @@
-module _pygfnfflib
+module gfnff
     use iso_fortran_env,only:wp => real64,stdout => output_unit
     use gfnff_interface
     implicit none
@@ -26,32 +26,28 @@ subroutine gfnff_sp(nat, ichrg, at, xyz, energy, grad, io)
     call gfnff_singlepoint(nat,at,xyz,calculator,energy,grad,pr,iostat=io)
 end subroutine gfnff_sp
 
-! subroutine gfnff_alpb(nat, ichrg, at, xyz, sol, energy, grad, io)
-!     integer,intent(in) :: nat
-!     integer,intent(in) :: sol
-!     integer,intent(in) :: ichrg
-!     integer,intent(in) :: at(nat)
-!     real(wp),intent(out) :: energy
-!     real(wp),intent(out) :: grad(nat, 3)
-!     real(wp),intent(in) :: xyz(nat, 3)
-!     integer,intent(out) :: io
+subroutine gfnff_alpb(nat, ichrg, at, xyz, sol, energy, grad, io)
+    integer,intent(in) :: nat
+    integer,intent(in) :: sol
+    integer,intent(in) :: ichrg
+    integer,intent(in) :: at(nat)
+    real(wp),intent(out) :: energy
+    real(wp),intent(out) :: grad(nat, 3)
+    real(wp),intent(in) :: xyz(nat, 3)
+    integer,intent(out) :: io
 
-!     !f2py intent(in) :: nat, ichrg, at, xyz, io, sol
-!     !f2py intent(out) :: energy, grad
-!     !f2py depend(nat) :: grad, xyz
+    !f2py intent(in) :: nat, ichrg, at, xyz, io, sol
+    !f2py intent(out) :: energy, grad
+    !f2py depend(nat) :: grad, xyz
 
-!     logical :: pr = .false.
-!     type(gfnff_data) :: calculator
-!     character(len=:),allocatable :: alpbsolvent
-!     if sol == 0 then
-!         alpbsolvent = 'h2o'
-!     else
-!         alpbsolvent = 'h2o'
-!     end if
+    logical :: pr = .false.
+    type(gfnff_data) :: calculator
+    character(len=:),allocatable :: alpbsolvent
+    alpbsolvent = 'h2o'
 
-!     !> calculation
-!     call gfnff_initialize(nat,at,xyz,calculator,print=pr,ichrg=ichrg,iostat=io)
-!     call gfnff_singlepoint(nat,at,xyz,calculator,energy,grad,pr,iostat=io)
-! end subroutine gfnff_alpb
+    !> calculation
+    call gfnff_initialize(nat,at,xyz,calculator,print=pr,ichrg=ichrg,iostat=io)
+    call gfnff_singlepoint(nat,at,xyz,calculator,energy,grad,pr,iostat=io)
+end subroutine gfnff_alpb
 
-end module _pygfnfflib
+end module gfnff
--- pygfnff/_pygfnff.py
+++ pygfnff/_pygfnff.py
@@ -1,3 +1,8 @@
-from pygfnff import _pygfnfflib  # type: ignore
+try:
+    from pygfnff import _pygfnfflib  # type: ignore
 
-print(help(_pygfnfflib))
+    print(help(_pygfnfflib))
+except ImportError:
+    from pygfnff._pygfnfflib import gfnff_sp  # type: ignore
+
+    print(help(gfnff_sp))
