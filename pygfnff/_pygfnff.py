ad3e508026330382a26acf00add6884e302198be
LiuGaoyong <liugaoyong_88@163.com>
2025-07-23 16:55:54 +0800

    update python wrapper file

--- pygfnff/_pygfnff.py
+++ pygfnff/_pygfnff.py
@@ -1,8 +1,31 @@
 try:
-    from pygfnff import _pygfnfflib  # type: ignore
+    import numpy as np
+    from ase.atoms import Atoms
 
-    print(help(_pygfnfflib))
+    from pygfnff import _pygfnfflib as lib  # type: ignore
+
+    def gfnff(atoms: Atoms) -> tuple[float, np.ndarray]:
+        """Run GFNFF calculation on a given ASE Atoms object."""
+        if atoms.pbc.any():
+            raise ValueError("Periodic boundary conditions are not supported.")
+
+        nat: int = len(atoms)
+        ichrg = int(atoms.get_initial_charges().sum())
+        result = lib.gfnff.gfnff_sp(
+            nat,
+            ichrg,
+            atoms.numbers.flatten(),
+            atoms.positions.flatten(),
+        )
+        print(result)
+        return result
+
 except ImportError:
-    from pygfnff._pygfnfflib import gfnff_sp  # type: ignore
+    raise ImportError("The pygfnff backend is not availible. ")
 
-    print(help(gfnff_sp))
+
+if __name__ == "__main__":
+    from ase.build import molecule
+
+    atoms = molecule("H2O")
+    gfnff(atoms)
